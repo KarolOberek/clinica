13449eb560c687d1456bc6a12be4417472249941
KarolineOberek <karolineoberek@gmail.com>
2024-06-25 10:32:37 -0300

    fixed adicionar

--- clinica.js
+++ clinica.js
@@ -19,44 +19,45 @@ process.stdin.on("data", function (data){
 if(!opcao){
     opcao = entrada
 
-} else if(opcao == "1" || opcao == 1){
+  if(opcao == "1"){
+    console.log("Já que deseja adicionar uma nova consulta, precisarei das seguintes informações:")
+    console.log("Qual o nome do paciente?")
+  }
+
+} else if(opcao == "1"){
     
     if (!consulta.nome_paciente){
-        console.log("Já que deseja adicionar uma nova consulta, precisarei das seguintes informações: ")
-        console.log("Qual o nome do paciente?")
         consulta.nome_paciente = entrada
+        console.log("Qual será o médico que vai atender o paciente?")
         
     } else if (!consulta.medico){
-        console.log("Qual será o médico que vai atender o paciente?")
+        
         consulta.medico = entrada
-
+        console.log("Qual o horário da consulta?")
+        
     } else if (!consulta.hora){
-        console.log("Qual o horário da consulta? ")
         consulta.hora = entrada
+        console.log("Qual vai ser o dia da consulta? (dia e mês apenas)")
 
     } else if (!consulta.data){
-        console.log("Qual vai ser o dia da consulta? (dia e mês apenas)")
+        
         consulta.data = entrada
-    
-    } else {
         consultas.push(consulta)
         opcao = undefined; 
         console.log("Consulta marcada com sucesso! ")
-        console.log("Caso queira continuar com o processo escolha uma das opções abaixo, caso não, digite sair")
-        console.log("opcão (1): Adicionar uma nova consulta")
-        console.log("opcão (2):Listar todas as consultas")
-        console.log("opcão (3):Atualizar uma consulta existente")
-        console.log("opcão (4):Cancelar uma consulta")
+        console.log("Caso queira continuar com o processo escolha uma das opções já fornecidas anteriormente, caso não, digite sair")
+    
+    } 
 
-    }
-
-} else if(opcao == "2" || opcao == 2){
+} else if(opcao == "2"){
     console.log(consultas)
+    opcao = undefined
+    console.log("Caso queira continuar com o processo escolha uma das opções já fornecidas anteriormente, caso não, digite sair")
 
-} else if(opcao == "3" || opcao == 3){
+} else if(opcao == "3"){
     console.log("3")
 
-} else if(opcao == "4" || opcao == 4){
+} else if(opcao == "4"){
     console.log("4")
 
 } else if(opcao == "sair"){
